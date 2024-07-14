ca11261942ca1413bb85a65f3a0cb6e2b82b8c39
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-07-14 12:00:30 +0530

    day2: operators

---
+++ day2/01operators.js
@@ -0,0 +1,33 @@
+// Addition
+let a =10;
+let b = 20;
+let ans =   a + b;
+console.log(ans)
+
+// Substraction
+a =60;
+b = 20;
+ans =   a - b;
+console.log(ans)
+
+// Multiplication
+a =6;
+b = 20;
+ans =   a * b;
+console.log(ans)
+
+// divide
+a =60;
+b = 20;
+ans =   a / b;
+console.log(ans)
+
+// Modulo
+a =60;
+b = 20;
+ans =   a % b;
+console.log(ans)
+
+number = -100;
+const result = number>0? "Positive" : "Negative"
+console.log(result)
