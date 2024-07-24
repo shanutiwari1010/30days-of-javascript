3a5a3a221053e1e210106459244f355d88bd34eb
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-07-24 15:54:48 +0530

    day 8: template literals

---
+++ day8/templateLiterals.js
@@ -0,0 +1,10 @@
+//  Use template literals to create a string that includes variables for a person's name and age, and log the string to the console.
+const name = "Shanu";
+const age = 22;
+const str = `My name is ${name} and age is ${age}`;
+console.log(str)
+
+// Create a multi-line string using template literals and log it to the console.
+const string = `bkdcbjkdvbnklkjnloewvbnm,mnb,mnm,mmmmmmmbkdcbjkdvbnklkjnloewvbnm,mnb,mnm,mmmmmmmbkdcbjkdvbnklkjnloewvbnm,mnb,mnm,mmmmmmmbkdcbjkdvbnklkjnloewvbnm,mnb,mnm,mmmmmmmvvvbkdcbjkdvbnklkjnloewvbnm,mnb,mnm,mmmmmmm`
+
+console.log(string)
